d9cac118b7e47abc613d8e82aaeab6466792d3e9
RIZAL NUGRAHANTO <rizalnxa929@gmail.com>
2025-01-06 02:46:21 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 6, 2025',
+  birthdate: 'Jul 2, 2025',
   name: 'LUSI'
 };
 
